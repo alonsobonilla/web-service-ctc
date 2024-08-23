4f156e680711f0df0aff78f69ae512fb88325f7f
Alonso Bonilla <luis.labp06@gmail.com>
2024-08-22 23:52:11 -0500

    add: new error for email and phone number duplicate

--- repositories/errors.go
+++ repositories/errors.go
@@ -3,6 +3,7 @@ package repositories
 import "errors"
 
 var (
-	ErrDuplicatedKeyEmail       = errors.New("duplicated email not allowed")
-	ErrDuplicatedKeyPhoneNumber = errors.New("duplicated phone_number not allowed")
+	ErrDuplicatedKeyEmail            = errors.New("correo ya registrado")
+	ErrDuplicatedKeyPhoneNumber      = errors.New("número de teléfono ya registrado")
+	ErrDuplicatedKeyEmailPhoneNumber = errors.New("número de teléfono y correo ya registrados")
 )

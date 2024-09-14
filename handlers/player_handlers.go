7a72b36fee972e2639edd874200509dfe9d5225c
Alonso Bonilla <luis.labp06@gmail.com>
2024-09-14 01:26:10 -0500

    change: errors manage of binding json and register player

--- handlers/player_handlers.go
+++ handlers/player_handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	playerdomain "ctcwebservice/core/player/domain"
 	playerports "ctcwebservice/core/player/ports"
+	handlers "ctcwebservice/handlers/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,11 +20,14 @@ func NewPlayerHandler(playerService playerports.PlayerService) *PlayerHandler {
 }
 func (h *PlayerHandler) Register(c *gin.Context) {
 	var player playerdomain.Player
-	c.BindJSON(&player)
+	if err := c.ShouldBindJSON(&player); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"data": nil, "message": err.Error()})
+		return
+	}
 
 	playerId, err := h.playerService.Register(&player)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(handlers.Mapped[err], gin.H{"data": nil, "error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": gin.H{"player_id": playerId}, "message": "Registrado correctamente"})

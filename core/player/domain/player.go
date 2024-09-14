2e89281650a33403e3e7f1256b27edc31643bac3
Alonso Bonilla <luis.labp06@gmail.com>
2024-09-14 01:10:16 -0500

    add: some validators

--- core/player/domain/player.go
+++ core/player/domain/player.go
@@ -7,31 +7,15 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// type Player struct {
-// 	PlayerID     uint
-// 	UserName     string    `json:"user_name"`
-// 	LastName     string    `json:"last_name"`
-// 	FirstName    string    `json:"first_name"`
-// 	Password     string    `json:"password"`
-// 	PhoneNumber  string    `json:"phone_number"`
-// 	Email        string    `json:"email"`
-// 	Gender       string    `json:"gender"`
-// 	BirthDate    time.Time `json:"birth_date"`
-// 	PlayerImage  string    `json:"player_image"`
-// 	CreatedAt    time.Time
-// 	UpdatedAt    time.Time
-// 	PlayerSports []sportdomain.Sport
-// }
-
 type Player struct {
 	PlayerID     uint   `gorm:"primaryKey"`
-	FirstName    string `gorm:"type:varchar(100);not null" json:"first_name"`
-	LastName     string `gorm:"type:varchar(100);not null" json:"last_name"`
-	Password     string `gorm:"type:varchar(255);not null" json:"password"`
-	PhoneNumber  string `gorm:"type:char(9);unique;not null" json:"phone_number"`
-	Email        string `gorm:"type:varchar(255);unique;not null" json:"email"`
-	Gender       string `gorm:"type:char(1);not null;" json:"gender"`
-	BirthDate    string `gorm:"type:date" json:"birth_date"`
+	FirstName    string `gorm:"type:varchar(100);not null" json:"first_name" binding:"required"`
+	LastName     string `gorm:"type:varchar(100);not null" json:"last_name" binding:"required"`
+	Password     string `gorm:"type:varchar(255);not null" json:"password" binding:"required"`
+	PhoneNumber  string `gorm:"type:char(9);unique;not null" json:"phone_number" binding:"required,len=9"`
+	Email        string `gorm:"type:varchar(255);unique;not null" json:"email" binding:"required,email"`
+	Gender       string `gorm:"type:char(1);not null;" json:"gender" binding:"required"`
+	BirthDate    string `gorm:"type:date" json:"birth_date" binding:"required"`
 	PlayerImage  string `json:"player_image"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time

2280ff9d53d05aeeeafcb87face147100a1e3e6e
Alonso Bonilla <luis.labp06@gmail.com>
2024-09-18 23:39:04 -0500

    change: add validators

--- core/subscription/domain/subscription.go
+++ core/subscription/domain/subscription.go
@@ -3,10 +3,10 @@ package subscriptiondomain
 import "time"
 
 type Subscription struct {
-	PlayerID   uint      `gorm:"primaryKey" json:"player_id"`
-	SportID    uint      `gorm:"primaryKey" json:"sport_id"`
-	Active     bool      `gorm:"type:boolean;not null" json:"active"`
-	LastSearch time.Time `gorm:"type:timestamp;not null"`
+	PlayerID   uint      `gorm:"primaryKey" json:"player_id" binding:"required,number"`
+	SportID    uint      `gorm:"primaryKey" json:"sport_id" binding:"required,number"`
+	Active     bool      `gorm:"type:boolean;not null;default:false" json:"active"`
+	LastSearch time.Time `gorm:"type:timestamp"`
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }

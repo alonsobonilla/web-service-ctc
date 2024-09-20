2d2ee743ecc237bef06f1856153643db73488aca
Alonso Bonilla <luis.labp06@gmail.com>
2024-09-19 23:44:21 -0500

    change: add subscription

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	authservice "ctcwebservice/core/auth/service"
 	playerservice "ctcwebservice/core/player/services"
+	subscriptionservice "ctcwebservice/core/subscription/services"
 	"ctcwebservice/handlers"
 	gormrepositorie "ctcwebservice/repositories/gorm_repositorie"
 	authgorm "ctcwebservice/repositories/gorm_repositorie/auth"
 	playergorm "ctcwebservice/repositories/gorm_repositorie/player"
+	subscriptionrepositorie "ctcwebservice/repositories/gorm_repositorie/subscription"
 	"ctcwebservice/server"
 	"ctcwebservice/utils"
 
@@ -24,7 +26,10 @@ func main() {
 	authService := authservice.NewAuthService(authRepository)
 	authHandlers := handlers.NewAuthHandler(authService)
 
-	server := server.NewServer(authHandlers, playerHandler)
+	subscriptionRepository := subscriptionrepositorie.NewSubscriptionRepository(gormrepositorie.Db)
+	subscriptionService := subscriptionservice.NewSubscriptionService(subscriptionRepository)
+	subscriptionHandlers := handlers.NewSubscriptionHandler(subscriptionService)
+	server := server.NewServer(authHandlers, playerHandler, subscriptionHandlers)
 
 	server.Initialize()
 }
